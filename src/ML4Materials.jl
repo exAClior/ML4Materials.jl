a54e000d70b58f0e0162e5c2ea94b043bb317e57
Yusheng Zhao <yushengzhao2020@outlook.com>
2023-09-18 18:07:55 +0800

    update for more general usecase

--- src/ML4Materials.jl
+++ src/ML4Materials.jl
@@ -5,6 +5,6 @@ using DSP, LinearAlgebra, Enzyme
 # Write your package code here.
 include("cnn.jl")
 include("cnn_utils.jl")
-export conv, _pad_1d
+export conv, _pad_1d, conv_sum
 
 end
--- src/cnn.jl
+++ src/cnn.jl
@@ -1,8 +1,13 @@
-function conv(input::Vector{T}, kernel::Vector{T}) where {T<:Number}
+function conv(input::Vector{T}, kernel::Vector{M}) where {T<:Number,M<:Number}
     paded_input = _pad_1d(input, length(kernel) ÷ 2)
-    output = zeros(T, length(input))
+    output = zeros(promote_type(T,M), length(input))
     for i = 1:length(input)
         output[i] = sum(view(paded_input, i:i+length(kernel)-1) .* kernel)
     end
     return output
+end
+
+function conv_sum(input::Vector{T}, kernel::Vector{M}) where {T<:Number, M<:Number}
+    output = conv(input, kernel)
+    return sum(output)
 end
